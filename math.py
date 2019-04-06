fa6271bdbcd56e0e8a61a4b95aa5347c23292e5a
Big Chungus <47265772+Streamerbtw2006@users.noreply.github.com>
2019-04-05 19:46:23 -0700

    Add files via upload

---
+++ math.py
@@ -0,0 +1,37 @@
+from myMath import Geometric
+from myMath import Arithmatic
+from myMath import area
+
+class Mathematic:
+
+    def __init__(self, arithmatic = Arithmatic(), geometric = Geometric()):
+        self._arithmatic = arithmatic
+        self._geometrics = geometric
+        
+    @property
+    def arithmatic(self):
+        return self._arithmatic
+
+    @property
+    def geometric(self):
+        return self._geometrics
+
+    def Add(self, x, y):
+        result = self.arithmatic.Addition(x, y)
+        print('Sum of two numbers: ' + str(result))
+           
+    def Multiply(self, x, y):
+        result = self.geometric.Multiplication(x, y)
+        print('Multiplication of two numbers: ' + str(result))
+        
+    def Subtraction(self, x, y):
+        result = self.arithmatic.Subtraction(x, y)
+        print('Difference of two numbers: ' + str(result))
+        
+    def Division(self, x, y):
+        result = self.geometric.Division(x, y)
+        print('Quotient of two numbers: ' + str(result))
+        
+    def Area(self, x, y):
+        result = self.geometric.Area(x, y)
+        print('Area of rectangle: ' + str(result))
